919deff6a58c39d0ea1313699c0444a620e1a978
czf <122385930@qq.com>
2020-12-08 16:38:24 +0800

    更改首页跳转到查询页

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import Demo from '@/pages/demo/App'
+// import Demo from '@/pages/demo/App'
 import Search from '@/pages/search'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
+        {/* <Route
             exact
             path="/"
             component={Demo}
-          />
+          /> */}
           <Route
             exact
             path="/search"
             component={Search}
           />
+          <Redirect from="/*" to="/search" />
       </Switch>
     </BrowserRouter>
   );
